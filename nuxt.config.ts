aa632336124af90d1fb93c11b7b06771b3dfae3f
Florian Joseph <flocamj@gmail.com>
2025-08-07 16:08:12 +0200

    before tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import Aura from '@primeuix/themes/aura';
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss'],
+  modules: ['@primevue/nuxt-module'],
   primevue: {
     autoImport: true,
     options: {
