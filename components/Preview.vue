ebab3c493b3db2bc964ebca6cea7f8a6492b08f6
Florian Joseph <flocamj@gmail.com>
2025-08-21 16:59:14 +0200

    update streamlink

---
+++ components/Preview.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="flex flex-col gap-4">
+        <div class="py-4">
+            <div class="flex flex-col text-center items-center h-24 justify-end">
+                <div class="font-bold text-4xl leading-tight">Aperçu</div>
+                <div class="text-xl leading-normal">
+                    Voici comment votre page apparaîtra à vos visiteurs
+                </div>
+            </div>
+        </div>
+
+        <!-- Aperçu du streamlink -->
+        <Card class="flex items-center h-160">
+            <template #title>
+                <div class="flex items-center text-center flex-col gap-2 mx-4 my-2">
+                    <Avatar :image="user.user_metadata.avatar_url" shape="circle" size="xlarge" />
+                    <span class="text-xl font-semibold">{{ user.user_metadata.nickname }}</span>
+                    <span class="text-sm text-surface-600">{{ user.user_metadata.custom_claims.description }}</span>
+                </div>
+            </template>
+            <template #content>
+                <div class="flex flex-col gap-2 text-center">
+                    <Card class="border-1">
+                        <template #content>
+                            <Twitch class="w-5 h-5 inline-block mr-2" />
+                            <span class="text-lg font-semibold">Twitch</span>
+                        </template>
+                    </Card>
+                    <Card class="border-1">
+                        <template #content>
+                            <Youtube class="w-5 h-5 inline-block mr-2" />
+                            <span class="text-lg font-semibold">YouTube</span>
+                        </template>
+                    </Card>
+                    <Card class="border-1">
+                        <template #content>
+                            <span class="text-lg font-semibold">TikTok</span>
+                        </template>
+                    </Card>
+                </div>
+            </template>
+            <template #footer>
+                <div class="flex justify-center mt-4">
+                    <Button severity="contrast">
+                        <span>Rejoignez {{ user.user_metadata.nickname }} sur StreamLink</span>
+                        <ArrowRight class="w-4 h-4" />
+                    </Button>
+                </div>
+            </template>
+        </Card>
+    </div>
+</template>
+
+<script setup>
+import { Twitch, Youtube, ArrowRight } from 'lucide-vue-next';
+
+const user = useSupabaseUser();
+</script>
---
+++ components/StreamLink.vue
@@ -0,0 +1,138 @@
+<template>
+    <div class="flex flex-col gap-4">
+        <div class="py-4">
+            <div class="flex flex-col items-center lg:items-start h-24 justify-end">
+                <div class="font-bold text-4xl leading-tight">Mon StreamLink</div>
+                <div class="text-xl leading-normal text-center lg:text-left">
+                    Créez votre page de liens personnalisée à partager avec votre audience
+                </div>
+            </div>
+        </div>
+
+        <!-- Aperçu du lien -->
+        <Card>
+            <template #title>
+                <div class="flex flex-col">
+                    <span class="text-lg font-semibold">Votre lien</span>
+                    <span class="text-sm text-gray-400 mt-1">
+                        Partagez ce lien avec votre audience pour qu'elle puisse vous suivre facilement.
+                    </span>
+                </div>
+            </template>
+            <template #content>
+                <div class="flex items-center bg-gray-100/10 rounded justify-between pr-4">
+                    <p class="m-4">
+                        streamlink.com/{{ user.user_metadata.nickname }}
+                    </p>
+                    <Files class="w-5 h-5 hover:text-blue-500 cursor-pointer" />
+                </div>
+            </template>
+        </Card>
+
+        <!-- Gérer les liens -->
+        <Card>
+            <template #title>
+                <div class="flex flex-col">
+                    <span class="font-semibold">Gérer vos liens</span>
+                    <span class="text-sm text-gray-400 mt-1">
+                        Ajoutez, modifiez ou supprimez vos liens pour les mettre à jour facilement.
+                    </span>
+                </div>
+            </template>
+            <template #content>
+                <div class="flex items-center gap-2">
+                    <Button severity="contrast">
+                        <Plus class="w-5 h-5" />
+                        <span>Ajouter un lien</span>
+                    </Button>
+                    <Button severity="contrast" @click="visible = true">
+                        <SquarePen class="w-5 h-5" />
+                        <span>Modifier la description</span>
+                    </Button>
+                </div>
+                <Dialog v-model:visible="visible" modal header="Nom et biographie" :style="{ width: '25rem' }">
+                    <div class="flex flex-col gap-4">
+                        <div class="flex flex-col gap-2">
+                            <label for="username">Nom</label>
+                            <InputText id="username" v-model="username" />
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <label for="bio">Biographie</label>
+                            <InputText id="bio" v-model="bio" />
+                        </div>
+                        <div class="flex justify-end gap-2">
+                            <Button type="button" label="Annuler" severity="secondary"
+                                @click="visible = false"></Button>
+                            <Button type="button" label="Sauvegarder" @click="visible = false"></Button>
+                        </div>
+                    </div>
+                </Dialog>
+
+            </template>
+            <template #footer>
+                <div class="flex flex-col gap-2">
+                    <Card class="border-1">
+                        <template #title>
+                            <Twitch class="w-5 h-5 inline-block mr-2" />
+                            <span class="text-lg font-semibold">Twitch</span>
+                        </template>
+                        <template #content>
+                            <div class="flex justify-between items-center">
+                                <p class="m-0 text-sm">
+                                    https://www.twitch.tv/nordik_saga
+                                </p>
+                                <div class="flex gap-2">
+                                    <Pencil class="w-5 h-5 cursor-pointer" />
+                                    <Trash2 class="w-5 h-5 cursor-pointer text-red-500" />
+                                </div>
+                            </div>
+                        </template>
+                    </Card>
+                    <Card class="border-1">
+                        <template #title>
+                            <span class="text-lg font-semibold">TikTok</span>
+                        </template>
+                        <template #content>
+                            <div class="flex justify-between items-center">
+                                <p class="m-0 text-sm">
+                                    https://www.tiktok.com/@nordik_saga
+                                </p>
+                                <div class="flex gap-2">
+                                    <Pencil class="w-5 h-5 cursor-pointer" />
+                                    <Trash2 class="w-5 h-5 cursor-pointer text-red-500" />
+                                </div>
+                            </div>
+                        </template>
+                    </Card>
+                    <Card class="border-1">
+                        <template #title>
+                            <Youtube class="w-5 h-5 inline-block mr-2" />
+                            <span class="text-lg font-semibold">YouTube</span>
+                        </template>
+                        <template #content>
+                            <div class="flex justify-between items-center">
+                                <p class="m-0 text-sm">
+                                    https://www.youtube.com/@NordiK_Saga
+                                </p>
+                                <div class="flex gap-2">
+                                    <Pencil class="w-5 h-5 cursor-pointer" />
+                                    <Trash2 class="w-5 h-5 cursor-pointer text-red-500" />
+                                </div>
+                            </div>
+                        </template>
+                    </Card>
+                </div>
+            </template>
+        </Card>
+    </div>
+</template>
+
+<script setup>
+import { Plus, Files, SquarePen, Pencil, Trash2, Twitch, Youtube } from 'lucide-vue-next';
+
+const user = useSupabaseUser()
+const visible = ref(false);
+const username = ref(user.value ? user.value.user_metadata.nickname : '');
+const bio = ref(user.value ? user.value.user_metadata.custom_claims.description : '');
+
+</script>
