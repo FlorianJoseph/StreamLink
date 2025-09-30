e5bd253e0bfad89765ef0fbaacfae2e1826271f6
Florian Joseph <flocamj@gmail.com>
2025-09-30 16:47:08 +0200

    1 composant for streamlinkedit

--- components/StreamLinkEdit.vue
+++ components/StreamLinkEdit.vue
@@ -1,123 +1,181 @@
+<!-- Layout en deux colonnes : édition à gauche, aperçu à droite -->
+
 <template>
-    <div class="flex flex-col gap-4">
+    <!-- Edition -->
+    <div class="flex-[2]">
+        <div class="flex flex-col gap-4">
 
-        <!-- En-tête -->
-        <div class="py-4">
-            <div class="flex items-center flex-col lg:items-start h-12 justify-end">
-                <h1 class="text-3xl font-bold">Modifier mon StreamLink</h1>
-                <p class="text-center lg:text-left">Créez votre page de liens personnalisée à partager avec votre
-                    audience</p>
-            </div>
-        </div>
-
-        <!-- Avatar et bouton de modification -->
-        <div class="flex flex-row gap-4 items-center">
-            <UploadAvatar />
-            <div>
-                <div class="flex flex-col" @click="visible = true">
-                    <span class="text-lg font-medium hover:underline cursor-pointer">
-                        {{ streamer?.username }}
-                    </span>
-                    <span class="text-sm text-gray-400 hover:underline cursor-pointer line-clamp-1">
-                        {{ streamer?.bio || 'Ajouter une description' }}
-                    </span>
+            <!-- En-tête -->
+            <div class="py-4">
+                <div class="flex items-center flex-col lg:items-start h-12 justify-end">
+                    <h1 class="text-3xl font-bold">Modifier mon StreamLink</h1>
+                    <p class="text-center lg:text-left">Créez votre page de liens personnalisée à partager avec
+                        votre
+                        audience</p>
                 </div>
-                <Dialog v-model:visible="visible" modal header="Nom et description" :style="{ width: '25rem' }">
-                    <div class="flex flex-col gap-4">
-                        <div class="flex flex-col gap-2">
-                            <label for="username">Nom</label>
-                            <InputText id="username" v-model="username" />
-                        </div>
-                        <div class="flex flex-col gap-2">
-                            <label for="description">Description</label>
-                            <Textarea id="description" v-model="bio" rows="5" cols="30" autoResize />
-                        </div>
-                        <div class="flex justify-end gap-2">
-                            <Button type="button" label="Annuler" severity="secondary"
-                                @click="visible = false"></Button>
-                            <Button type="button" label="Sauvegarder" severity="contrast" @click="handleSave"></Button>
-                        </div>
-                    </div>
-                </Dialog>
             </div>
-        </div>
 
-        <!-- Aperçu du lien -->
-        <Card>
-            <template #title>
-                <div class="flex items-center justify-between">
-                    <div class="flex flex-col">
-                        <span class="text-lg font-semibold">Votre lien</span>
-                        <span class="text-sm text-gray-400">
-                            Partagez ce lien avec votre audience pour qu'elle puisse vous suivre facilement.
+            <!-- Avatar et bouton de modification -->
+            <div class="flex flex-row gap-4 items-center">
+                <UploadAvatar />
+                <div>
+                    <div class="flex flex-col" @click="visible = true">
+                        <span class="text-lg font-medium hover:underline cursor-pointer">
+                            {{ streamer?.username }}
+                        </span>
+                        <span class="text-sm text-gray-400 hover:underline cursor-pointer line-clamp-1">
+                            {{ streamer?.bio || 'Ajouter une description' }}
                         </span>
                     </div>
-                    <NuxtLink :to="{ path: `/${streamer?.username}` }" target="_blank">
-                        <Button class="whitespace-nowrap w-full" severity="contrast" variant="outlined">
-                            <span>Voir mon StreamLink</span>
-                            <ExternalLink class="w-4 h-4" />
-                        </Button>
-                    </NuxtLink>
-                </div>
-            </template>
-            <template #content>
-                <div class="flex items-center bg-gray-100/10 rounded justify-between pr-4">
-                    <p class="m-4">
-                        streamlink.com/{{ streamer?.username }}
-                    </p>
-                    <Files class="w-5 h-5 hover:text-blue-500 cursor-pointer transition-colors" />
-                </div>
-            </template>
-        </Card>
-
-        <!-- Gérer les liens -->
-        <Card>
-            <template #title>
-                <div class="flex flex-col">
-                    <span class="font-semibold">Gérer vos liens</span>
-                    <span class="text-sm text-gray-400">
-                        Ajoutez, modifiez ou supprimez vos liens pour les mettre à jour facilement.
-                    </span>
-                </div>
-            </template>
-            <template #content>
-                <div class="flex flex-col gap-2">
-
-                    <Button severity="contrast" class="w-full">
-                        <Plus class="w-5 h-5" />
-                        <span class="font-medium">Ajouter un lien</span>
-                    </Button>
-
-                    <!-- Exemple de lien -->
-                    <div class="rounded-lg p-4 bg-gray-100/10 text-white">
-                        <!-- Header -->
-                        <div class="flex items-center gap-2 mb-2">
-                            <span class="text-lg font-semibold">Twitch</span>
-                            <Pencil class="w-4 h-4 cursor-pointer" />
+                    <Dialog v-model:visible="visible" modal header="Nom et description" :style="{ width: '25rem' }">
+                        <div class="flex flex-col gap-4">
+                            <div class="flex flex-col gap-2">
+                                <label for="username">Nom</label>
+                                <InputText id="username" v-model="username" />
+                            </div>
+                            <div class="flex flex-col gap-2">
+                                <label for="description">Description</label>
+                                <Textarea id="description" v-model="bio" rows="5" cols="30" autoResize />
+                            </div>
+                            <div class="flex justify-end gap-2">
+                                <Button type="button" label="Annuler" severity="secondary"
+                                    @click="visible = false"></Button>
+                                <Button type="button" label="Sauvegarder" severity="contrast"
+                                    @click="handleSave"></Button>
+                            </div>
                         </div>
+                    </Dialog>
+                </div>
+            </div>
 
-                        <!-- Content -->
-                        <div class="flex justify-between items-center">
-                            <div class="flex gap-2 items-center">
-                                <p class="m-0 text-sm">
-                                    https://www.twitch.tv/nordik_saga
-                                </p>
+            <!-- Aperçu du lien -->
+            <Card>
+                <template #title>
+                    <div class="flex items-center justify-between">
+                        <div class="flex flex-col">
+                            <span class="text-lg font-semibold">Votre lien</span>
+                            <span class="text-sm text-gray-400">
+                                Partagez ce lien avec votre audience pour qu'elle puisse vous suivre facilement.
+                            </span>
+                        </div>
+                        <NuxtLink :to="{ path: `/${streamer?.username}` }" target="_blank">
+                            <Button class="whitespace-nowrap w-full" severity="contrast" variant="outlined">
+                                <span>Voir mon StreamLink</span>
+                                <ExternalLink class="w-4 h-4" />
+                            </Button>
+                        </NuxtLink>
+                    </div>
+                </template>
+                <template #content>
+                    <div class="flex items-center bg-gray-100/10 rounded justify-between pr-4">
+                        <p class="m-4">
+                            streamlink.com/{{ streamer?.username }}
+                        </p>
+                        <Files class="w-5 h-5 hover:text-blue-500 cursor-pointer transition-colors" />
+                    </div>
+                </template>
+            </Card>
+
+            <!-- Gérer les liens -->
+            <Card>
+                <template #title>
+                    <div class="flex flex-col">
+                        <span class="font-semibold">Gérer vos liens</span>
+                        <span class="text-sm text-gray-400">
+                            Ajoutez, modifiez ou supprimez vos liens pour les mettre à jour facilement.
+                        </span>
+                    </div>
+                </template>
+                <template #content>
+                    <div class="flex flex-col gap-2">
+
+                        <Button severity="contrast" class="w-full">
+                            <Plus class="w-5 h-5" />
+                            <span class="font-medium">Ajouter un lien</span>
+                        </Button>
+
+                        <!-- Exemple de lien -->
+                        <div class="rounded-lg p-4 bg-gray-100/10 text-white">
+                            <!-- Header -->
+                            <div class="flex items-center gap-2 mb-2">
+                                <span class="text-lg font-semibold">Twitch</span>
                                 <Pencil class="w-4 h-4 cursor-pointer" />
                             </div>
-                            <div class="flex gap-2">
-                                <Trash2 class="w-5 h-5 cursor-pointer hover:text-red-500 transition-colors" />
+
+                            <!-- Content -->
+                            <div class="flex justify-between items-center">
+                                <div class="flex gap-2 items-center">
+                                    <p class="m-0 text-sm">
+                                        https://www.twitch.tv/nordik_saga
+                                    </p>
+                                    <Pencil class="w-4 h-4 cursor-pointer" />
+                                </div>
+                                <div class="flex gap-2">
+                                    <Trash2 class="w-5 h-5 cursor-pointer hover:text-red-500 transition-colors" />
+                                </div>
                             </div>
                         </div>
-                    </div>
 
+                    </div>
+                </template>
+            </Card>
+        </div>
+    </div>
+
+    <!-- Aperçu -->
+    <div class="flex-[1]">
+        <div class="flex flex-col gap-4">
+            <div class="py-4">
+                <div class="flex items-center flex-col h-12 justify-end">
+                    <h1 class="text-3xl font-bold">Aperçu</h1>
+                    <p class="text-center lg:text-left">Voici comment votre page apparaîtra à vos visiteurs</p>
                 </div>
-            </template>
-        </Card>
+            </div>
+
+            <!-- Aperçu du streamlink -->
+            <Card class="flex flex-col h-160 w-97">
+                <template #title>
+                    <div class="mb-8 w-full">
+                        <div class="flex justify-between w-full">
+                            <Button rounded severity="secondary">
+                                <Home class="w-4 h-4" />
+                            </Button>
+                            <Button rounded severity="secondary">
+                                <span class="text-sm">Copier le lien</span>
+                                <Files class="w-4 h-4" />
+                            </Button>
+                        </div>
+                    </div>
+                    <div class="flex items-center text-center flex-col w-full my-6">
+                        <Avatar
+                            :image="streamer?.avatar_url || 'https://vcvwxwhiltffzmojiinc.supabase.co/storage/v1/object/public/Avatar/default/avatar.png'"
+                            shape="circle" size="xlarge" class="mb-2" />
+                        <span class="text-lg font-semibold">{{ streamer?.username }}</span>
+                        <span class="text-sm font-medium">
+                            {{ streamer?.bio }}
+                        </span>
+                    </div>
+                </template>
+                <template #content>
+                    <div class="flex flex-col gap-2 w-full">
+                        <LinkCard />
+                    </div>
+                </template>
+                <template #footer>
+                    <div class="flex justify-center mt-4 w-full">
+                        <Button severity="contrast">
+                            <span>Rejoignez {{ streamer?.username }} sur StreamLink</span>
+                            <ArrowRight class="w-4 h-4" />
+                        </Button>
+                    </div>
+                </template>
+            </Card>
+        </div>
     </div>
 </template>
 
 <script setup>
-import { Plus, Files, Pencil, Trash2, ExternalLink } from 'lucide-vue-next';
+import { Plus, Files, Pencil, Trash2, ExternalLink, ArrowRight, Home } from 'lucide-vue-next';
 
 const visible = ref(false);
 const username = ref('')
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -8,12 +8,7 @@
             <StreamLinkCreate @created="onStreamerCreated" />
         </div>
         <div v-else class="flex flex-col lg:flex-row lg:gap-48 gap-4">
-            <div class="flex-[2]">
-                <StreamLinkEdit />
-            </div>
-            <div class="flex-[1]">
-                <StreamLinkPreview />
-            </div>
+            <StreamLinkEdit />
         </div>
     </div>
 </template>
