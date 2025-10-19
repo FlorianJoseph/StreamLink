bfef2814d0286a65b2033ed8fa1b5e38e76c1a56
Florian Joseph <flocamj@gmail.com>
2025-10-19 15:20:45 +0200

    pnpm for build and remove prisma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@primevue/nuxt-module', '@nuxtjs/supabase', '@prisma/nuxt', '@pinia/nuxt', '@nuxt/icon'],
+  modules: ['@primevue/nuxt-module', '@nuxtjs/supabase', '@pinia/nuxt', '@nuxt/icon'],
   primevue: {
     autoImport: true,
     options: {
