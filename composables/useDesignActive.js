7224b12df59199dac2f54888ef5137c8c219b492
Florian Joseph <flocamj@gmail.com>
2025-12-30 21:00:34 +0100

    fix null theme at fetch

--- composables/useDesignActive.js
+++ composables/useDesignActive.js
@@ -1,6 +1,6 @@
 const shallowEqual = (a = {}, b = {}) => {
-    const aKeys = Object.keys(a)
-    const bKeys = Object.keys(b)
+    const aKeys = Object.keys(a ?? {})
+    const bKeys = Object.keys(b ?? {})
 
     if (aKeys.length !== bKeys.length) return false
 
