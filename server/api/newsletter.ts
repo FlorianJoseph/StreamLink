601f9baa16390f0ce4092d7e54e8e46a7c48b985
Florian Joseph <flocamj@gmail.com>
2026-01-08 18:40:16 +0100

    template resend

--- server/api/newsletter.ts
+++ server/api/newsletter.ts
@@ -1,43 +1,39 @@
 import { serverSupabaseClient } from '#supabase/server'
-import { newsletterTemplate } from '~/server/templates/newsletter'
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.NUXT_RESEND_API_KEY);
 
 export default defineEventHandler(async (event) => {
   const supabase = await serverSupabaseClient(event)
-  const { emails } = useResend();
 
   // Récupère les abonnés
   const { data: subscribers, error } = await supabase
-    .from('newsletter_subscriptions_with_email')
-    .select('email')
+    .from('newsletter_subscriptions')
+    .select('email, username')
     .eq('subscribed', true)
   console.log(subscribers);
 
-  if (error) throw createError({ statusCode: 500, message: error.message })
-
-  if (!subscribers || subscribers.length === 0) {
-    return { sent: 0 }
-  }
-
-  // Sujet & contenu définis côté serveur
-  const subject = '✨ Nouveau : personnalise ton StreamLink !'
-  const content = ''
+  if (error) throw createError({ statusCode: 500, message: error.message });
+  if (!subscribers || subscribers.length === 0) return { sent: 0 };
 
   // Envoi des emails
-
-  const result = await emails.send({
-    from: 'StreamLink <newsletter@mail.stream-link.fr>',
-    // to: subscribers.map(s => s.email),
-    to: ['flocamj@gmail.com'],
-    subject,
-    html: newsletterTemplate(content),
-  })
+  const result = await resend.batch.send(
+    subscribers.map(s => ({
+      from: 'StreamLink <newsletter@mail.stream-link.fr>',
+      to: s.email,
+      // to: ['flocamj@gmail.com'],
+      subject: '',
+      template: {
+        id: 'update',
+        variables: {
+          username: s.username,
+        },
+      },
+    }))
+  );
 
   if (result.error) {
-    console.error('Erreur Resend:', result.error)
-    throw createError({
-      statusCode: 500,
-      message: 'Erreur lors de l’envoi de la newsletter',
-    })
+    throw createError({ statusCode: 500, message: 'Erreur lors de l\'envoi' });
   }
 
   return { sent: subscribers.length }
